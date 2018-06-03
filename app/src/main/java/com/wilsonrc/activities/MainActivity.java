3dfc28da6c7ae7ca82b3cd1fa4df9b6c1b87e1f5
wilsonrc <wilsonrc16@gmail.com>
2018-06-03 16:57:21 -0400

    Update Profile Screen

--- app/src/main/java/com/wilsonrc/activities/MainActivity.java
+++ app/src/main/java/com/wilsonrc/activities/MainActivity.java
@@ -1,13 +1,35 @@
 package com.wilsonrc.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppCompatButton buttonLogin;
+    EditText inputEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputEmail = findViewById(R.id.input_email);
+        buttonLogin = findViewById(R.id.btn_login);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = "";
+                if(inputEmail!=null){
+                    email = inputEmail.getText().toString();
+                }
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                intent.putExtra("NAME", email);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/wilsonrc/activities/ProfileActivity.java
+++ app/src/main/java/com/wilsonrc/activities/ProfileActivity.java
@@ -2,12 +2,26 @@ package com.wilsonrc.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView textViewName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        textViewName = findViewById(R.id.textViewName);
+        String name = getIntent().getExtras().getString("NAME");
+        if(textViewName!= null){
+            textViewName.setText(name);
+        }
+
     }
+
+
 }
