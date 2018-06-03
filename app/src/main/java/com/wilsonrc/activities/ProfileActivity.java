c90bf94ab598682ce3e2540469a661230adb5aad
wilsonrc <wilsonrc16@gmail.com>
2018-06-03 17:17:06 -0400

    Add App Screen shots

--- app/src/main/java/com/wilsonrc/activities/ProfileActivity.java
+++ app/src/main/java/com/wilsonrc/activities/ProfileActivity.java
@@ -14,7 +14,7 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        setTitle("Profile");
         textViewName = findViewById(R.id.textViewName);
         String name = getIntent().getExtras().getString("NAME");
         if(textViewName!= null){
